82bfeec85d5b02435e3c7997defb4e00a9555468
Sam <samilalghiffary@gmail.com>
2024-05-02 14:06:19 +0700

    feat: Add new icons

--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -1,3 +1,7 @@
 export * from "./Bars3Icon";
 export * from "./ChevronDownIcon";
 export * from "./ExternalLinkIcon";
+export * from "./FloodInformationIcon";
+export * from "./MagnifyingGlassIcon";
+export * from "./ShieldIcon";
+export * from "./WarningIcon";
