9463c824854ee6001a83cff285f15c10b5d2f259
anggasptr <saputraa170503@gmail.com>
2024-05-04 17:50:36 +0700

    fix: changes url footer to coming soon page

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 
+import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
 import { Button } from "@/components/ui";
 
@@ -37,6 +38,7 @@ export default function NotFound() {
           />
         </div>
       </div>
+      <Footer />
     </>
   );
 }
