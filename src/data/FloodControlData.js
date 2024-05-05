bedc2917f70770046be631435e8d2a2472a4fb01
Sam <samilalghiffary@gmail.com>
2024-05-06 01:32:22 +0700

    change flood control data to javascript array of objects

---
+++ src/data/FloodControlData.js
@@ -0,0 +1,77 @@
+export const FloodControlData = [
+  {
+    id: 1,
+    authorities: "pemerintah",
+    data: [
+      {
+        id: 1,
+        slug: "sistem-pengendalian-banjir",
+        title: "Sistem Pengendalian Banjir",
+        paragraph:
+          "Sistem pengendalian banjir di Jakarta dari sistem drainase utama, sistem drainase kedua, dan sistem waduk-pompa. Sistem drainase utama sendiri dibagi kembali menjadi Banjir Kanal Barat dan Banjir Kanal Timur. Hujan lokal pun akan dialirkan secara gravitasi ke sistem drainase kedua yang kemudian dibuang ke sistem utama hingga berakhir di laut. Sistem waduk-pompa membantu menyedot air yang menggenang dan mengembalikannya ke aliran sungai",
+      },
+      {
+        id: 2,
+        slug: "pompa-air",
+        title: "Pompa Air",
+        paragraph:
+          "Dinas Sumber Daya Air (DSDA) mengelola 495 unit pompa air stasioner dan 327 unit pompa mobile yang tersebar di DKI Jakarta. Fungsi utama pompa-pompa ini adalah untuk memompa air dari tempat rendah ke tempat tinggi. Sehingga dapat memitigasi banjir dengan menyedot air yang menggenang dan mengalirkannya ke tempat yang seharusnya, seperti sungai.",
+      },
+      {
+        id: 3,
+        slug: "alat-berat",
+        title: "Alat Berat",
+        paragraph:
+          "Alat berat digunakan untuk mitigasi banjir yang dilakukan Provinsi DKI Jakarta, seperti pengerukan waduk/situ/embung, kali/sungai, dan saluran air. Per 20 September 2021, Pemprov DKI Jakarta memiliki 228 unit alat berat.",
+      },
+      {
+        id: 4,
+        slug: "gerebek-lumpur",
+        title: "Gerebek Lumpur",
+        paragraph:
+          "Alat berat digunakan untuk mitigasi banjir yang dilakukan Provinsi DKI Jakarta, seperti pengerukan waduk/situ/embung, kali/sungai, dan saluran air. Per 20 September 2021, Pemprov DKI Jakarta memiliki 228 unit alat berat.",
+      },
+      {
+        id: 5,
+        slug: "drainase-vertikal",
+        title: "Drainase Vertikal",
+        paragraph:
+          "Salah satu metode penanganan banjir yang digunakan oleh Jakarta adalah pembangunan drainase vertikal, dikenal juga sebagai sumur resapan. Selain untuk menampung air, daya serap drainase vertikal juga dapat membantu mempercepat peresapan air ke dalam tanah.",
+      },
+      {
+        id: 6,
+        slug: "jakpantau",
+        title: "JakPantau",
+        paragraph:
+          "JakPantau merupakan salah satu fitur di aplikasi JAKI milik Pemprov DKI Jakarta. Fitur ini akan memudahkanmu mendapatkan segala informasi banjir. ",
+      },
+    ],
+  },
+  {
+    id: 2,
+    authorities: "masyarakat",
+    data: [
+      {
+        id: 1,
+        slug: "kerja-bakti",
+        title: "Kerja Bakti",
+        paragraph:
+          "Kerja bakti merupakan salah satu wujud dari kegiatan gotong- royong antar warga masyarakat untuk menjaga kebersihan lingkungan sekitar. Tujuan dari kegiatan kerja bakti adalah untuk menjaga agar lingkungan tetap bersih dan menjaga kelestarian lingkungan serta sumber daya alam yang ada.",
+      },
+      {
+        id: 2,
+        slug: "drainase-vertikal",
+        title: "Drainase Vertikal",
+        paragraph:
+          "Drainase vertikal merupakan upaya mengelola air hujan dengan cara ditampung dan diresapkan ke dalam tanah secara alamiah, sehingga tidak hanya bisa mengatasi banjir tapi juga dapat menjadi cadangan air saat musim kemarau. Inisiatif masyarakat untuk membangun sumur resapan akan semakin meningkatkan efektivitas dan intensitas penyerapan air ke tanah.",
+      },
+      {
+        id: 2,
+        slug: "lapor-banjir",
+        title: "Lapor Banjir",
+        paragraph:
+          "Ikut berpartisipasi dalam menangani banjir dengan melaporkan genangan atau banjir di wilayah Jakarta melalui aplikasi JAKI",
+      },
+    ],
+  },
+];
