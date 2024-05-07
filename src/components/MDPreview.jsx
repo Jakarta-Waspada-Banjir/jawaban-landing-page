fd0bf1fc0de7fbc2bbe6049bc98e4e24d5c91203
Sam <samilalghiffary@gmail.com>
2024-05-07 12:09:26 +0700

    fix: Markdown preview

--- src/components/MDPreview.jsx
+++ src/components/MDPreview.jsx
@@ -1,15 +1,11 @@
 "use client";
 
-import DOMPurify from "dompurify";
-
 export const MDPreview = ({ value = "" }) => {
-  const purify = DOMPurify(window);
-  const sanitizedHTML = purify.sanitize(value);
   return (
     <div
-      className="prose w-full"
+      className="w-full"
       dangerouslySetInnerHTML={{
-        __html: sanitizedHTML,
+        __html: value,
       }}
     />
   );
