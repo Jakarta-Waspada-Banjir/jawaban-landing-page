ecd4af328e8624a7c8dde48cbc6e8f08d1ac7b65
anggasptr <saputraa170503@gmail.com>
2024-05-12 18:47:53 +0700

    feat: faq

--- src/components/ui/index.jsx
+++ src/components/ui/index.jsx
@@ -8,6 +8,7 @@ export * from "./collapsible";
 export * from "./dropdown-menu";
 export * from "./input";
 export * from "./label";
+export * from "./select";
 export * from "./separator";
 export * from "./sheet";
 export * from "./tabs";
